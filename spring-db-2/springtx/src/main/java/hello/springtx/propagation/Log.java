d6de1629a723262beb3ca01f39199a06975631f4
iamminseongKim <minseongkimim@gmail.com>
2024-10-22 08:24:47 +0900

    스프링 트랜잭션 전파 2 활용 - 트랜잭션 전파 활용3 - 단일 트랜잭션

---
+++ spring-db-2/springtx/src/main/java/hello/springtx/propagation/Log.java
@@ -0,0 +1,23 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Log {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String message;
+
+    public Log(String message) {
+        this.message = message;
+    }
+
+    public Log() {}
+}
